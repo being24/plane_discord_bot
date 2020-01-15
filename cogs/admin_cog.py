71db0dabc9dde3ba030cde817a452ea63dcb3272
being241 <forprom.sn.16@gmail.com>
2020-01-15 21:29:51 +0900

    admin追加

---
+++ cogs/admin_cog.py
@@ -0,0 +1,65 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+import subprocess
+import typing
+
+import discord
+from discord.ext import commands  # Bot Commands Frameworkのインポート
+
+
+def is_in_guild():
+    async def predicate(ctx):
+        return ctx.guild and ctx.guild.id == 609058923353341973
+    return commands.check(predicate)
+
+
+def is_owner():
+    async def predicate(ctx):
+        return ctx.author.id == 277825292536512513
+    return commands.check(predicate)
+
+
+class Tachibana_admin(commands.Cog):  # コグとして用いるクラスを定義。
+
+    def __init__(self, bot):  # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+        self.bot = bot
+        self.master_path = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))
+
+    @commands.command(aliases=['re'], hidden=True)
+    @is_owner()
+    @is_in_guild()
+    async def reload(self, ctx):
+        for cog in self.bot.INITIAL_COGS:
+            try:
+                self.bot.unload_extension(f"{cog}")
+                self.bot.load_extension(f"{cog}")
+                await ctx.send(f"{cog} reloaded")
+            except Exception as e:
+                print(e)
+
+    @reload.error
+    async def reload_error(self, ctx, error):
+        await ctx.send(f'to <@{self.bot.admin_id}> at {ctx.command.name} command\n{error}')
+
+    @commands.command(aliases=['st'], hidden=True)
+    @is_owner()
+    @is_in_guild()
+    async def status(self, ctx, word: str):
+        try:
+            await self.bot.change_presence(activity=discord.Game(name=word))
+            await ctx.send(f"ステータスを{word}に変更しました")
+            self.bot.status = word
+        except BaseException:
+            pass
+
+    @status.error
+    async def status_error(self, ctx, error):
+        await ctx.send(f'to <@{self.bot.admin_id}> at {ctx.command.name} command\n{error}')
+
+
+def setup(bot):  # Bot本体側からコグを読み込む際に呼び出される関数。
+    bot.add_cog(Tachibana_admin(bot))  # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
