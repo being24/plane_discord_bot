5f3420b30bf252bc7b29921bf0d73f5a11a87434
being <34680324+being24@users.noreply.github.com>
2023-05-21 22:15:03 +0900

    update

--- cogs/utils/common.py
+++ cogs/utils/common.py
@@ -1,36 +1,38 @@
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import logging
 import typing
+
 import discord
 
 
-class CommonUtil():
+class CommonUtil:
     def __init__(self):
         pass
 
     @staticmethod
-    async def autodel_msg(msg: discord.Message, second: int = 5):
+    async def delete_after(msg: discord.Message | discord.InteractionMessage, second: int = 5):
         """渡されたメッセージを指定秒数後に削除する関数
 
         Args:
             msg (discord.Message): 削除するメッセージオブジェクト
             second (int, optional): 秒数. Defaults to 5.
         """
-        try:
-            await msg.delete(delay=second)
-        except discord.Forbidden:
-            pass
+        if isinstance(msg, discord.InteractionMessage):
+            try:
+                await msg.delete(delay=second)
+            except discord.Forbidden:
+                logging.error("メッセージの削除に失敗しました。Forbidden")
+        else:
+            try:
+                await msg.delete(delay=second)
+            except discord.Forbidden:
+                logging.error("メッセージの削除に失敗しました。Forbidden")
 
     @staticmethod
-    def return_member_or_role(guild: discord.Guild,
-                              id: int) -> typing.Union[discord.Member,
-                                                       discord.Role,
-                                                       None]:
+    def return_member_or_role(guild: discord.Guild, id: int) -> typing.Union[discord.Member, discord.Role, None]:
         """メンバーか役職オブジェクトを返す関数
 
         Args:
-            guild (discord.guild): discordのguildオブジェクト
+            guild (discord.guild): discordpyのguildオブジェクト
             id (int): 役職かメンバーのID
 
         Returns:
